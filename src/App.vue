f835b49f97161de923fb6e3749d70cb177faf01d
David <kanter.dev@gmail.com>
2020-08-20 18:15:37 -0400

    added some connection stuffs

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,96 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <div class="container">
+            <h1>Status: {{ status }}</h1>
+            <h3>Your Peer ID: {{ peerID || 'Fetching...' }}</h3>
+            <h3 class=""></h3>
+            <label>
+                Target Unique ID
+                <input type="text" v-model="targetID"/>
+            </label>
+            <button>Connect</button>
+            <div class="call-container">
+                <video class="remote-video" autoplay/>
+                <video class="local-video" autoplay muted/>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+import Peer from 'peerjs';
+
+const CONN_DETAILS = {
+    host: '68.183.197.101',
+    port: 9000,
+    key: 'connect_me'
+}
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: {},
+    async created() {
+        this.peerID = await this.getUniqueID();
+        console.log(this.peerID);
+        this.connectPeer();
+    },
+    data: () => ({
+        peer: null,
+        peerID: null,
+        connectedPeers: [],
+        targetID: '',
+        status: 'Offline.'
+    }),
+    methods: {
+        async getUniqueID() {
+            const url = `http://${CONN_DETAILS.host}:${CONN_DETAILS.port}/${CONN_DETAILS.key}/id`;
+            const res = await axios.get(url);
+            return res.data;
+        },
+        async getConnectedPeers() {
+            const url = `http://${CONN_DETAILS.host}:${CONN_DETAILS.port}/${CONN_DETAILS.key}/peers`;
+            const res = await axios.get(url);
+            return res.data;
+        },
+        connectPeer() {
+            const peer = new Peer(this.peerID, {
+                ...CONN_DETAILS,
+                debug: 3
+            });
+
+            const timer = setInterval(checkConnection, 2000);
+
+            const checkConnection = async () => {
+                if (peer.disconnected)
+                    peer.reconnect();
+                else
+                    this.connectedPeers = await this.getConnectedPeers();
+            }
+
+            peer.on('open', id => {
+                this.status = 'Standing By For Connections.';
+            });
+
+            peer.on('close', id => {
+                this.status = 'Offline.';
+            });
+
+            peer.on('disconnected', () => {
+                this.status = 'Disconnected.';
+            });
+        }
+    }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
